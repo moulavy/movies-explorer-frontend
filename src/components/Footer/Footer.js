441d6f2cd8e317053bc12d0a756431427b70263d
Milana Kulieva <72352010+moulavy@users.noreply.github.com>
2023-06-09 14:58:26 +0300

    footer html

--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -1,11 +1,18 @@
 import React from 'react';
-
-function Portfolio() {
+import './Footer.css'
+function Footer() {
    return (
-      <section className="portfolio">
-         
-      </section>
+      <footer className="footer">
+         <p className="footer__description">Учебный проект Яндекс.Практикум х BeatFilm.</p>
+         <div className="footer__content">
+            <p className="footer__year">2023</p>
+            <ul className="footer__list">
+               <li className="footer__list-item"><a href="https://practicum.yandex.ru/">Яндекс.Практикум</a></li>
+               <li className="footer__list-item"><a href="https://github.com/moulavy">Github</a></li>
+            </ul>
+         </div>
+      </footer>
    );
 }
 
-export default Portfolio;
+export default Footer;
