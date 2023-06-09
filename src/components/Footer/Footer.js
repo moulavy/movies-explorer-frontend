12858919a69d8d77afa4b93493172ea790dd945c
Milana Kulieva <72352010+moulavy@users.noreply.github.com>
2023-06-09 16:23:07 +0300

    статический футер

--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -5,10 +5,10 @@ function Footer() {
       <footer className="footer">
          <p className="footer__description">Учебный проект Яндекс.Практикум х BeatFilm.</p>
          <div className="footer__content">
-            <p className="footer__year">2023</p>
+            <p className="footer__year">© 2023</p>
             <ul className="footer__list">
-               <li className="footer__list-item"><a href="https://practicum.yandex.ru/">Яндекс.Практикум</a></li>
-               <li className="footer__list-item"><a href="https://github.com/moulavy">Github</a></li>
+               <li className="footer__list-item"><a className="footer__link" href="https://practicum.yandex.ru/">Яндекс.Практикум</a></li>
+               <li className="footer__list-item"><a className="footer__link" href="https://github.com/moulavy">Github</a></li>
             </ul>
          </div>
       </footer>
