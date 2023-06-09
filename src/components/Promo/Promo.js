df962b61648bad5fdcb0f5d1351ab5c04c884d47
Milana Kulieva <72352010+moulavy@users.noreply.github.com>
2023-06-09 13:36:25 +0300

    fixed:promo под pixel perfect

--- src/components/Promo/Promo.js
+++ src/components/Promo/Promo.js
@@ -1,12 +1,12 @@
 // компонент с вёрсткой баннера страницы «О проекте»
 import React from 'react';
-import './Promo.css'
+import './Promo.css';
+import logoImg from '../../images/logo-bg.svg'
 function Promo() {
    return (
-      <section className="promo">
-         <div className="promo__container">
-            <h1 className="promo__title">Учебный проект студента факультета Веб-разработки.</h1>
-         </div>
+      <section className="promo">         
+            <img className="promo__logo" src={logoImg} alt="лого"/>
+            <h1 className="promo__title">Учебный проект студента факультета Веб-разработки.</h1>         
       </section>
 
    );
