ba40a5f3eab4e40c90f1cffdbdeac49d0f4eb5ae
Milana Kulieva <72352010+moulavy@users.noreply.github.com>
2023-06-10 21:30:58 +0300

    header+burger

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,17 +1,20 @@
-import React from 'react';
+import React,{useState} from 'react';
 import './Header.css';
 import logoImg from '../../images/logo.svg';
 import iconProfileImg from '../../images/iconProfile.svg'
 import { Link } from 'react-router-dom';
 const isLoggedIn = true;
 const isOpen = false;
+
 function Header() {
+   const [menu, setMenu] = useState(false);
    return (
       <header className={(isLoggedIn ? 'header header-movies' : 'header header-main')}>
          <div className={(isLoggedIn ? 'header__container header__container-movies' : 'header__container header__container-main')}>
             <img src={logoImg} alt="лого" />
+            <button onClick={() => setMenu(!menu)} className={(menu ? "header__burger-close" : "header__burger-menu")}></button>
             {isLoggedIn ? (
-               <div className="header__content">
+               <div className={(menu ? "header__content header__content_open" : "header__content")} >
                   <nav className="header__movies-section">
                      <Link className='header__movies header__home'>Главная</Link>
                      <Link className='header__movies header__movies_enabled'>Фильмы</Link>
@@ -20,7 +23,7 @@ function Header() {
                   <div className="header__profile">
                   <Link className='header__profile-text'>Аккаунт</Link>
                      <img className='header__profile-img' src={iconProfileImg} alt="иконка профиля" />
-                     <button className={(isOpen ? "header__burger-close" : "header__burger-menu")}></button>
+                     
                   </div>
 
                </div>
