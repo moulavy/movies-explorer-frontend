4d8118d8f9348b091d84caba7dfe75e07ef6b1e0
Milana Kulieva <72352010+moulavy@users.noreply.github.com>
2023-06-11 00:47:35 +0300

    FilterCheckBox сделан switch checkbox

--- src/components/FilterCheckbox/FilterCheckBox.js
+++ src/components/FilterCheckbox/FilterCheckBox.js
@@ -1,1 +1,16 @@
 //фильтр с чекбоксом «Только короткометражки».
+import React from 'react';
+import './FilterCheckBox.css';
+
+function FilterCheckBox() {
+   return (
+      
+         <div class="checkbox">
+            <input type="checkbox" id="filter" className="checkbox__input"/>
+               <label htmlFor="filter" className="checkbox__label"></label>
+         </div>
+      
+   );
+}
+
+export default FilterCheckBox;
--- src/components/SearchForm/SearchForm.js
+++ src/components/SearchForm/SearchForm.js
@@ -1,12 +1,20 @@
 //форма поиска, куда пользователь будет вводить запрос
 import React from 'react';
 import './SearchForm.css';
-
+import searchIcon from '../../images/searchIcon.svg';
+import FilterCheckBox from '../FilterCheckbox/FilterCheckBox.js';
 function SearchForm() {
    return (
-      <>
-        
-      </>
+      <section className="search">
+         <form className="search__form">
+            <img className="search__icon" alt="иконка поиска" src={searchIcon} />
+            <input className="search__input" type="text" placeholder='Фильм' />
+            <button type="submit" className="searchButton">Найти</button>
+            <FilterCheckBox />
+            
+
+        </form>
+      </section>
    );
 }
 
