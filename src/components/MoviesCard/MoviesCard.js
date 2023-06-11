ed9b525c1021463b2b9ef153082ac9f31669f5fb
Milana Kulieva <72352010+moulavy@users.noreply.github.com>
2023-06-11 17:50:44 +0300

    карточки адаптив

--- src/components/MoviesCard/MoviesCard.js
+++ src/components/MoviesCard/MoviesCard.js
@@ -1,13 +1,18 @@
 //компонент одной карточки фильма
-
 import React from 'react';
 import './MoviesCard.css';
-
+import movieImg from '../../images/movieImg.png';
 function MoviesCard() {
+   const isLiked = true;
    return (
-      <>
-
-      </>
+      <div className="movie">
+         <img src={movieImg} alt="изображение фильма" className="movie__img" />
+         <div className="movie__content">
+            <h2 className="movie__title">33 слова о дизайне</h2>            
+            <button className={(isLiked ? "movie__like movie__like_active" : "movie__like movie__like_inactive")}></button>
+         </div>
+         <p className="movie__time">1ч 42м</p>
+      </div>
    );
 }
 
--- src/components/MoviesCardList/MoviesCardList.js
+++ src/components/MoviesCardList/MoviesCardList.js
@@ -1,12 +1,31 @@
 //компонент, который управляет отрисовкой карточек фильмов на страницу и их количеством
 import React from 'react';
 import './MoviesCardList.css';
+import MoviesCard from '../MoviesCard/MoviesCard.js';
 
 function MoviesCardList() {
    return (
-      <>
-
-      </>
+      <section className="movieslist">
+         <div className="movieslist__container">
+            <MoviesCard /> 
+            <MoviesCard /> 
+            <MoviesCard /> 
+            <MoviesCard /> 
+            <MoviesCard /> 
+            <MoviesCard /> 
+            <MoviesCard /> 
+            <MoviesCard /> 
+            <MoviesCard />
+            <MoviesCard />
+            <MoviesCard />
+            <MoviesCard />
+            <MoviesCard />
+            <MoviesCard />
+            <MoviesCard />
+            <MoviesCard /> 
+         </div>
+        
+      </section>
    );
 }
 
