1ac94a6155b40af812369b61a7ae1d5aee3fa091
Milana Kulieva <72352010+moulavy@users.noreply.github.com>
2023-06-12 18:21:11 +0300

    profile верстка

--- src/components/Profile/Profile.js
+++ src/components/Profile/Profile.js
@@ -6,26 +6,29 @@ import { Link } from 'react-router-dom';
 function Profile({ name }) {
    const isLoggedIn = true;
    return (
-      <section className="profile">
-         <div className="profile__container">
-            <Header isLoggedIn={isLoggedIn} />
-            <h1 className="profile__title">Привет, {name}!</h1>
-            <form className="profile__wrapper">
-               <div className="profile__group">
-                  <label className="profile__label" htmlFor="name">Имя</label>
-                  <input autoComplete="off" type="text" id="name" className="profile__input form__name" name="name"
-                     minLength="2" maxLength="35" required value="Виталий"/>                 
-               </div>
-               <div className="profile__group">
-                  <label className="profile__label" htmlFor="email">E-mail</label>
-                  <input autoComplete="off" type="email" id="email" className="profile__input form__email" name="email"
-                     required value="pochta@yandex.ru" />
-               </div>
-               <button type="submit" className="profile__button">Редактировать</button>
-            </form>
-            <Link to="/" className="profile__logout">Выйти из аккаунта</Link>
-         </div>
-      </section>
+      <>
+         <Header isLoggedIn={isLoggedIn} />
+         <section className="profile">
+            <div className="profile__container">
+
+               <h1 className="profile__title">Привет, {name}!</h1>
+               <form className="profile__wrapper">
+                  <div className="profile__group profile__name">
+                     <label className="profile__label" htmlFor="name">Имя</label>
+                     <input autoComplete="off" type="text" id="name" className="profile__input profile__input-name" name="name"
+                        minLength="2" maxLength="35" required value="Виталий" />
+                  </div>
+                  <div className="profile__group profile__email">
+                     <label className="profile__label" htmlFor="email">E-mail</label>
+                     <input autoComplete="off" type="email" id="email" className="profile__input profile__input-email" name="email"
+                        required value="pochta@yandex.ru" />
+                  </div>
+                  <button type="submit" className="profile__button">Редактировать</button>
+               </form>
+               <Link to="/" className="profile__logout">Выйти из аккаунта</Link>
+            </div>
+         </section>
+      </>
    );
 }
 
