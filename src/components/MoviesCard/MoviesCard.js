486d3778ea1ba270a154182f0f27612c76c2c8bd
Milana Kulieva <72352010+moulavy@users.noreply.github.com>
2023-06-20 23:09:51 +0300

    feat:трейлер по клику на карточку

--- src/components/MoviesCard/MoviesCard.js
+++ src/components/MoviesCard/MoviesCard.js
@@ -1,46 +1,54 @@
-//компонент одной карточки фильма
-import React, { useState } from 'react';
+import React from 'react';
+import { Link } from 'react-router-dom';
 import './MoviesCard.css';
-function MoviesCard({ onDeleteMovie, onAddMovie, movie, isPageSavedMovies, saveMovies }) {
 
-   const isLiked = isPageSavedMovies ? true : saveMovies.some(savedMovie => savedMovie.movieId === movie.id);
+function MoviesCard({ onDeleteMovie, onAddMovie, movie, isPageSavedMovies, saveMovies }) {
+   const isLiked = isPageSavedMovies
+      ? true
+      : saveMovies.some((savedMovie) => savedMovie.movieId === movie.id);
+
    const onLike = (e) => {
       e.preventDefault();
       if (!isLiked) {
-         onAddMovie(movie)
-      }
-      else {
-         const movieToDelete = saveMovies.find(item => movie.id === item.movieId);
+         onAddMovie(movie);
+      } else {
+         const movieToDelete = saveMovies.find((item) => movie.id === item.movieId);
          onDeleteMovie(movieToDelete);
       }
-   }
+   };
 
-   const deleteMovie = () => {     
-      onDeleteMovie(movie);     
-   }
-
+   const deleteMovie = () => {
+      onDeleteMovie(movie);
+   };
 
    const durationTransform = (duration) => {
       const hour = Math.floor(duration / 60);
       const minutes = duration % 60;
       if (hour !== 0) {
          return `${hour}ч ${minutes}м`;
-      }
-      else {
+      } else {
          return `${minutes}м`;
       }
-
-   }
-
+   };
 
    return (
       <div className="movie">
-         <img src={isPageSavedMovies ? movie.image : `https://api.nomoreparties.co/${movie.image.url}`} alt={movie.nameRU} className="movie__img" />
+         <Link to={movie.trailerLink} target="_blank">
+            <img
+               src={isPageSavedMovies ? movie.image : `https://api.nomoreparties.co/${movie.image.url}`}
+               alt={movie.nameRU}
+               className="movie__img"
+            />
+         </Link>
          <div className="movie__content">
             <h2 className="movie__title">{movie.nameRU}</h2>
-            {(isPageSavedMovies ?
-               (<button onClick={ deleteMovie} className="movie__button movie__delete"></button>) :
-               (<button onClick={onLike} className={(isLiked ? "movie__button movie__like_active" : "movie__button movie__like_inactive")}></button>)
+            {isPageSavedMovies ? (
+               <button onClick={deleteMovie} className="movie__button movie__delete"></button>
+            ) : (
+               <button
+                  onClick={onLike}
+                  className={isLiked ? 'movie__button movie__like_active' : 'movie__button movie__like_inactive'}
+               ></button>
             )}
          </div>
          <p className="movie__time">{durationTransform(movie.duration)}</p>
